2f6c3d4cb6c20661c2a14ac905f3b99e1095f5ec
LeonardoRk <leodegolim@yahoo.com.br>
2019-10-22 15:06:03 -0300

    users' Attributes Insertion Test

--- src/test/java/br/com/parebem/reservas/test/TestUser.java
+++ src/test/java/br/com/parebem/reservas/test/TestUser.java
@@ -1,27 +1,41 @@
 package br.com.parebem.reservas.test;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+
+import org.junit.Test;
 
 import br.com.parebem.reservas.modelo.User;
+import br.com.parebem.reservas.util.JpaUtil;
 
-public class TestaUser {
+public class TestUser {
 
-	public static void main(String[] args) {
+	@Test
+	public void usersAttributesInsertionTest() {
+		String name = "Leonardo";
+		String telephone = "99887878";
+		String password = "senhasenha";
+		
 		User user = new User();
-		user.setNome("Leonardo");
-		user.setTelefone("9988987786");
+		user.setName(name);
+		user.setTelephone(telephone);
+		user.setPassword(password);
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("novo_controle");
-		EntityManager em = emf.createEntityManager();
-		
+		EntityManager em = new JpaUtil().getEntityManager();
 		em.getTransaction().begin();
 		em.persist(user);
+		
+		User insertedUser = em.find(User.class, user.getUser_id());
+		
+		em.remove(insertedUser);
 		em.getTransaction().commit();
 		
 		em.close();
-		emf.close();
 		
+		assertEquals(insertedUser.getName(), name);
+		assertEquals(insertedUser.getTelephone(), telephone);
+		assertEquals(insertedUser.getPassword(), password);
 	}
+	
 }
